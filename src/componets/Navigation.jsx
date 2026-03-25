2c04a406d0d0bfc887a451c1c1287c928c95567c
oben11 <dithskii@gmail.com>
2026-03-25 18:55:28 -0400

    Reimplemented toggle nav

--- src/componets/Navigation.jsx
+++ src/componets/Navigation.jsx
@@ -1,27 +1,28 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../css/Navigation.css";
+import { useState } from "react";
 
 
 
 const Navigation = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-    <header>
-        <nav>
-            <section>
-                <Link to="/home" class="logo">Ollie's</Link>
-            </section>
-            <section>
-                <script src="toggleNav.js" defer></script>
-                <button id="hamburger" class="hamburger">☰</button>
-                <ul>
+        <header>
+            <nav>
+                <div className="nav-top">
+                    <Link to="/home" className="logo">Ollie's</Link>
+                    <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>☰</button>
+                </div>
+
+                <ul className={menuOpen ? "nav-links open" : "nav-links"}>
                     <li><Link to="/menu">Menu</Link></li>
                     <li><Link to="/rewards">Rewards</Link></li>
                     <li><Link to="/about">About Us</Link></li>
                     <li><Link to="/merchandise">Merchandise</Link></li>
                 </ul>
-            </section>
-        </nav>
-    </header>
+            </nav>
+        </header>
     );
 };
 
