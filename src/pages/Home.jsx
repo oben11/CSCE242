63f0a19c5efb018e8b47b1286753b110e2c705ec
oben11 <dithskii@gmail.com>
2026-03-25 20:55:34 -0400

    fixed home splash image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import "../css/Home.css";
 import Features from "../componets/home/Features";
 import SignatureRoast from "../componets/home/SignatureRoast";
 import Hero from "../componets/Hero"
-
+import home_splash from "../media/home_splash.jpg"
 
 const Home = () => {
     return (
@@ -12,7 +12,7 @@ const Home = () => {
         <Hero
         title="Welcome to Ollie's Coffee Company"
         description="Crafting the perfect cup since 1987."
-        backgroundImage="./media/home_splash.jpg"
+        backgroundImage={home_splash}
         animate={true}
         buttons={[
             { label: "Order Now", href: "menu.html", style: "primary-dark" },
